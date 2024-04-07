db4ca579eba8af6cb92d0be093a59e4d349efb2b
AurelDeveloper <marlon.profils@gmail.com>
2024-04-07 12:43:22 +0200

    Debug index.py so its run every 2 hours with github actions

--- api/index.py
+++ api/index.py
@@ -1,12 +1,5 @@
-from flask import Flask
-from api.utils.weather import fetch_and_store_weather_data
+def run_script():
+    with open('./utils/weather.py', 'r') as file:
+        exec(file.read())
 
-app = Flask(__name__)
-
-@app.route('/fetch_weather')
-def fetch_weather():
-    fetch_and_store_weather_data()
-    return 'Weather data fetched and stored successfully.'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+run_script()
